b3ae8539c81cd401f1596f418051a197ec9363af
Randy <ztelur@gmail.com>
2022-03-03 17:47:07 +0800

    update
    Signed-off-by: Randy <ztelur@gmail.com>

--- components/encryption/src/io.rs
+++ components/encryption/src/io.rs
@@ -132,6 +132,10 @@ impl<W> EncrypterWriter<W> {
     pub fn finalize(self) -> IoResult<W> {
         self.0.finalize()
     }
+
+    pub fn get_inner(&self) -> &W {
+        &self.0.writer
+    }
 }
 
 impl<W: Write> Write for EncrypterWriter<W> {
